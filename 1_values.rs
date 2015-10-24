7b11dcd19b66686ea69bbedf7fbac7f6d5b78cd9
Pouya Gharib Pour <p.gharibpour@gmail.com>
2015-10-24 17:53:39 +0330

    finish values example

---
+++ 1_values.rs
@@ -0,0 +1,6 @@
+fn main() {
+  println!("{}", "a string".to_string()); // String
+  println!("this is an str"); // str
+  println!("{}", 3.14 * 6.45); // float (f64)
+  println!("{}", true || true); // bool
+}

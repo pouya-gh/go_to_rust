72f38271b75328422a35bfd30c8e83c6b2858bcd
Pouya Gharib Pour <p.gharibpour@gmail.com>
2015-10-24 18:04:55 +0330

    add more examples to values example

--- 1_values.rs
+++ 1_values.rs
@@ -2,5 +2,10 @@ fn main() {
   println!("{}", "a string".to_string()); // String
   println!("this is an str"); // str
   println!("{}", 3.14 * 6.45); // float (f64)
+  // rust's numeric types:
+  // i8, i16, i32, i64 (signed integer)
+  // u8, u16, u32, u64 (unsigned integer)
+  // isize, usize
+  // f32, f64 (float)
   println!("{}", true || true); // bool
 }

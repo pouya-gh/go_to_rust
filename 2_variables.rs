3f24e48172503a1e53fee88f32bfc77c60d24d78
Pouya Gharib Pour <p.gharibpour@gmail.com>
2015-10-24 18:16:43 +0330

    explain about mutability in variables example

--- 2_variables.rs
+++ 2_variables.rs
@@ -1,7 +1,10 @@
 // define a variable with 'let'
 fn main() {
-  let a: String = String::new();
-  let b: i32 = 26;
-  let c = 3.14; // there is no need to specify the type. rust can figure it out. (here: f64)
+  // variables are immutable by default.
+  let a: String = String::new(); // an immutable String 
+  let b: i32 = 26; // an immutable 32 bit integer
+  // there is no need to specify the type. rust can figure it out. 
+  let mut c = 3.14; // a mutable 64 bit float.
+  
   println!("{}\n{}\n{}", a, b, c);
 }
